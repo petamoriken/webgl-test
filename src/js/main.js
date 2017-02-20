d724cf44c6aa6b9f4c55c18a9acb4d354fb5cc6d
petamoriken <moriken@kimamass.com>
2017-02-21 03:44:25 +0900

    update fetch glsl

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
 import { draw } from "./draw";
 
 (async () => {
+    const fetchVertex = fetch("glsl/main.vert").then(req => req.text());
+    const fetchFragment = fetch("glsl/main.frag").then(req => req.text());
+
     const waitLoading = new Promise(resolve => {
         window.addEventListener("DOMContentLoaded", resolve, { capture: false, once: true });
     });
 
-    const vertex = await fetch("glsl/main.vert").then(req => req.text());
-    const fragment = await fetch("glsl/main.frag").then(req => req.text());
-
-    await waitLoading;
+    const [vertex, fragment] = await Promise.all([fetchVertex, fetchFragment, waitLoading]);
 
     const canvas = document.getElementById("canvas");
     const gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
